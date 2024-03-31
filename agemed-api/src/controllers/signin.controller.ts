9692be5ade944392a3d0d25b36100a523512fa0a
livia araujo <liviaaraujo.dev@gmail.com>
2024-03-31 12:50:58 -0300

    feat: sign in

---
+++ agemed-api/src/controllers/signin.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { SignInService } from "../services/sign.in.service";
+
+export class SignInController {
+
+    async signIn(request: Request, response: Response) {
+        const { body } = request;
+
+        try {
+            const signInControoler = new SignInService();
+            const result = await signInControoler.execute(body);
+            return response.json(result);
+        } catch (err: any) {
+            return response.status(400).json({
+                error: err.message,
+            });
+        }
+    }
+}
